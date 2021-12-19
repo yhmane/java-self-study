572161ba9e340361ba26c08e82771dce6ef64137
yhmane <hwang9yh@gmail.com>
2021-12-19 19:57:08 +0900

    interface Compartator 변경

--- src/main/java/compare/interfaceComparator/ComparatorMain.java
+++ src/main/java/compare/interfaceComparator/ComparatorMain.java
@@ -1,6 +1,5 @@
 package compare.interfaceComparator;
 
-
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
@@ -16,15 +15,7 @@ public class ComparatorMain {
 
         List list = Arrays.asList(s1, s2, s3, s4);
 
-        Collections.sort(list, (Comparator<Student>) (o1, o2) -> {
-            if (o1.getAge() > o2.getAge()) {
-                return 1;
-            } else if (o1.getAge() == o2.getAge()) {
-                return 0;
-            } else {
-                return -1;
-            }
-        });
+        Collections.sort(list, Comparator.comparingInt(Student::getAge));
 
         list.forEach(System.out::println);
     }
