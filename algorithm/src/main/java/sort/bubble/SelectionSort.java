fa624c7828e51b698f903a98b2659cda0a459556
황윤호 <hwang9yh@wemakeprice.com>
2022-03-10 11:46:17 +0900

    선택정렬 - 시간복잡도 O(n2)

--- algorithm/src/main/java/sort/bubble/SelectionSort.java
+++ algorithm/src/main/java/sort/bubble/SelectionSort.java
@@ -3,7 +3,7 @@ package sort.bubble;
 import java.util.Arrays;
 
 // O(n2)
-public class BubbleSort {
+public class SelectionSort {
 
     public static void main(String[] args) {
         int[] num =  {10, 5, 8, 3, 2, 1, 9};
@@ -12,14 +12,19 @@ public class BubbleSort {
 
     public static void sort(int[] numbers) {
         for (int i = 0; i < numbers.length - 1; i++) {
+            int least = i;
             for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] > numbers[j]) {
-                    int temp = numbers[i];
-                    numbers[i] = numbers[j];
-                    numbers[j] = temp;
+                if (numbers[j] < numbers[least]) {
+                    least = j;
                 }
             }
-        }
+
+            if (i != least) {
+                int temp = numbers[i];
+                numbers[i] = numbers[least];
+                numbers[least] = temp;
+            }
+         }
 
         System.out.println(Arrays.toString(numbers));
     }
